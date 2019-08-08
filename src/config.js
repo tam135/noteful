e0a965da179131c51aca39c85e14148737c32deb
Tam Phan <tnphan0204@gmail.com>
2019-08-08 21:18:36 +0100

    updated url to connect with api server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT_FOLDER: 'http://localhost:8000/api/folder',
-  API_ENDPOINT_NOTE: 'http://localhost:8000/api/note',
+  API_ENDPOINT_FOLDER: 'https://salty-temple-11746.herokuapp.com/api/folder',
+  API_ENDPOINT_NOTE: 'https://salty-temple-11746.herokuapp.com/api/note',
   API_KEY: process.env.REACT_APP_API_KEY
 }
